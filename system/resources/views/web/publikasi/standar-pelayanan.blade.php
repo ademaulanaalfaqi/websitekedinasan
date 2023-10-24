73c15c665da317240488f3d0f6175cec5d52e3ca
Ade Maulana Alfaqi <107929610+ademaulanaalfaqi@users.noreply.github.com>
2023-10-24 08:51:24 +0700

    commit

--- system/resources/views/web/publikasi/standar-pelayanan.blade.php
+++ system/resources/views/web/publikasi/standar-pelayanan.blade.php
@@ -1,48 +1,66 @@
 @extends('components.base.base')
 @section('content')
-<div class="sidebar-page-container">
-    <div class="auto-container">
-        <div class="row clearfix">
+    <!--Title-->
+    <section class="page-title" style="background-image:url({{ url('public') }}/assets/images/background/17580.jpg);">
+        <div class="auto-container">
+            <h1>Standar Pelayanan {{$config['singkatan_website']}}</h1>
+        </div>
 
-            <!--Content Side-->
-            <div class="content-side col-lg-12 col-md-8 col-sm-12 col-xs-12">
-                <section class="news-outer">
-                    <div class="sec-title-one">
-                        <h2 style="font-size: 26px">Sandar Pelayanan {{$config['singkatan_website']}}</h2> <br>
+        <!--page-info-->
+        <div class="page-info">
+            <div class="auto-container">
+                <div class="row clearfix">
+
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                        <ul class="bread-crumb clearfix">
+                            <li><a href="{{ url('/') }}">Home</a></li>
+                            <li class="active">Standar Pelayanan</li>
+                        </ul>
                     </div>
-                    <div class="news-style-two">
-                        <div class="inner-box">
-                    
-                            <!--lower-content-->
-                            <div class="lower-content">
-                                <table class="table">
-                                    <thead>
-                                      <tr>
-                                        <th scope="col" height="10px">No</th>
-                                        <th scope="col">First</th>
-                                      </tr>
-                                    </thead>
-                                    <tbody>
-                                      <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                
-                            </div>
-                    
-                        </div>
-                    </div>
-                        
-                
-                </section>
-            
+
+                </div>
             </div>
-            <!--Content Side-->
+        </div>
 
+    </section>
+    <div class="sidebar-page-container">
+        <div class="auto-container">
+            <div class="row clearfix">
 
+                <!--Content Side-->
+                <div class="content-side col-lg-12 col-md-8 col-sm-12 col-xs-12">
+                    <section class="news-outer">
+                        <div class="sec-title-one" style="margin-bottom: 0px">
+                            <h2 style="font-size: 26px">Sandar Pelayanan {{ $config['singkatan_website'] }}</h2> <br>
+                        </div>
+                        <div class="news-style-two">
+                            <div class="inner-box">
+
+                                <!--lower-content-->
+                                <div class="lower-content">
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col" style="width: 100px">No</th>
+                                                <th scope="col">Nama Dokumen</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">1</th>
+                                                <td>Mark</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <!--Content Side-->
+
+            </div>
         </div>
     </div>
-</div>
 @endsection
